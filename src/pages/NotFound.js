b330143719e586874cc0a043b44fb740c9939b76
Amar <augustine2903a@gmail.com>
2022-07-24 00:53:28 -0400

    update notFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,23 @@
-import * as React from "react";
+
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 
 // markup
 const NotFound = () => {
+	const navigate = useNavigate()
+
+  useEffect(() => {
+    setTimeout(() => {
+     navigate('/')
+    }, [1500])
+    
+  }, [navigate])
 	return (
 		<main className="not-found-container">
 			<h1> Sorry! This page does not exist.</h1>
+		
 		</main>
+		
 	);
 };
 
